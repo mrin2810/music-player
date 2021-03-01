549f0cf76e2b84def25b51e3616eba1de4760f59
mrinmayee.rane <mrinmayee.rane@zeuslearning.com>
2021-03-02 02:06:19 +0530

    added progress bar updation

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progress = document.getElementById('progress');
+const durationElem = document.getElementById('duration');
+const currTime = document.getElementById('current-time');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
 let songNum = 0;
 const songs = [
     {
@@ -61,9 +65,22 @@ function nextSong() {
     playSong();
 }
 
+// Update Progress bar and Time
+
+function updateProgressBar(e) {
+    if(isPlaying){
+        const {duration, currentTime} = e.srcElement;
+        const progressPercent = currentTime / duration * 100;
+        console.log(progressPercent);
+        progress.style.width = `${progressPercent}%`;
+    }
+}
+
+
 playBtn.addEventListener('click', () => {(isPlaying ? pauseSong() : playSong())});
 nextBtn.addEventListener('click', nextSong);
 prevBtn.addEventListener('click', prevSong);
+music.addEventListener('timeupdate', updateProgressBar);
 
 // Update DOM 
 
@@ -73,4 +90,4 @@ function loadSong(song) {
     music.setAttribute('src', `music/${song.name}.mp3`);
 }
 
-loadSong(songs[3])
+loadSong(songs[0]);
